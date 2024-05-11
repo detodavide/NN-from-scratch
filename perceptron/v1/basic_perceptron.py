1eacdb61aa79c8c9779e0293dff937695dadf721
detodavide <davide.marchetti931@gmail.com>
2024-05-11 12:45:15 +0200

    fix

--- perceptron/v1/basic_perceptron.py
+++ perceptron/v1/basic_perceptron.py
@@ -2,12 +2,12 @@ class Perceptron():
     def __init__(self, inputs, weights, bias) -> None:
         self.output: float = 0.0
         self.inputs: list[float] = inputs
-        self.weight: list[float] = weights
+        self.weights: list[float] = weights
         self.bias: float = bias
 
     def forward(self) -> None:
         # sum inputs to their weights
-        for idx, input in enumerate(self.inputs):
-            out = input*self.weight[idx]
+        for input, weight in zip(self.inputs, self.weights):
+            out = input*weight
             self.output += out
         self.output += self.bias
